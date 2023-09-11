611589a556020c6e349953a8b6cba8b7bb773a47
Daniel Di Domenico <daniel@daniel.com>
2023-09-11 14:00:59 -0300

    Aula 10 - CRUD MVC

---
+++ implementacoes/crud_alunos/util/config.php
@@ -0,0 +1,7 @@
+<?php
+
+//Configurar essas variáveis de acordo com o seu ambiente
+define("DB_HOST", "mysql-server");
+define("DB_NAME", "crud_alunos");
+define("DB_USER", "root");
+define("DB_PASSWORD", "root");
